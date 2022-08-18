23d2cb56e27af00ac68433a643924d0013ae99b3
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-18 13:33:53 +0700

    Summ of digits_Init commit

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Math.Abs (Convert.ToInt32 (Console.ReadLine ()));
+int sum = 0;
+int derivateNumber = number;
+    while (derivateNumber !=0)
+        {
+            sum = sum + derivateNumber % 10;        // Каждый ход цикла брем цифру из порядка единиц производного числа и прибавляем его к сумме цифр.
+            derivateNumber = derivateNumber / 10;   // Кадный ход цикла убираем цифру, которая находилась в порядке единиц производного числа. Условие выхода из цикла - равенство производного ила нулю
+        }
+Console.WriteLine($"Сумма цифр числа {number} равна {sum}");
+
+
