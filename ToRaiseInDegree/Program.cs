854620149bbb618873510f161e8a1a4964df0e0e
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-18 12:26:42 +0700

    Raise in degree_Init comit

---
+++ ToRaiseInDegree/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите степень, в которую требуется возвести число");
+int degree = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine (Math.Pow(number, degree));
